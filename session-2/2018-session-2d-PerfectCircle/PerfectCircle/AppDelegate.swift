12abe6d2cf8cf40aaf416b58ac9292994de6add7
tabinks <abinkowski@uchicago.edu>
2020-04-14 19:38:58 -0500

    Update session 2

---
+++ session-2/2018-session-2d-PerfectCircle/PerfectCircle/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PerfectCircle
+//
+//  Created by T. Andrew Binkowski on 4/9/18.
+//  Copyright © 2018 T. Andrew Binkowski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
